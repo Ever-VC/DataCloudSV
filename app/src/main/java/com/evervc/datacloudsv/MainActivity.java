3684fb38ee7092e152258894c3da15e765904861
Ever-VC <ever.vasquezc27@gmail.com>
2025-04-14 23:22:58 -0600

    :lipstick: Modo claro y oscuro a la barra de navegación inferior

--- app/src/main/java/com/evervc/datacloudsv/MainActivity.java
+++ app/src/main/java/com/evervc/datacloudsv/MainActivity.java
@@ -1,6 +1,9 @@
 package com.evervc.datacloudsv;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.evervc.datacloudsv.ui.HomeActivity;
+
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar prgsbrLoadActivity;
+    private Handler handler = new Handler();
+    private int progressStatus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bindElemtsXml();
+        simulateProgress();
+    }
+
+    private void bindElemtsXml() {
+        prgsbrLoadActivity = findViewById(R.id.prgsbrLoadActivity);
+    }
+
+    private void simulateProgress() {
+        new Thread(() -> {
+            while (progressStatus < 100) {
+                progressStatus += 1;
+                handler.post(() -> prgsbrLoadActivity.setProgress(progressStatus));
+                try {
+                    Thread.sleep(20); // Velocidad del progreso
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // Inicia HomeActivity una vez completado el progreso
+            Intent homeActivity = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(homeActivity);
+            finish();
+        }).start();
     }
 }
